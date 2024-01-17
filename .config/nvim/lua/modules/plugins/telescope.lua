5101dacc7ce61d6f5593cb4513153418bff937cc
Evgenii Gostev <gostevevg0@gmail.com>
2024-01-17 10:53:42 +0300

    Change live_grep to live_grep_with_args

--- .config/nvim/lua/modules/plugins/telescope.lua
+++ .config/nvim/lua/modules/plugins/telescope.lua
@@ -6,7 +6,7 @@ telescope['nvim-telescope/telescope.nvim'] = {
     init = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end,
