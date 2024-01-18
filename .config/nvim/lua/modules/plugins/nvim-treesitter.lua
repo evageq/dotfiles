e20df26fb91fb10daa9488e605295aca347cd388
Evgenii Gostev <gostevevg0@gmail.com>
2024-01-18 12:16:49 +0300

    plugins/nvim-treesitter: fix setup config

--- .config/nvim/lua/modules/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/modules/plugins/nvim-treesitter.lua
@@ -3,26 +3,16 @@ local nv_tree = {}
 nv_tree['nvim-treesitter/nvim-treesitter'] = {
     build = ":TSUpdate",
     main = 'nvim-treesitter.configs',
-    config = require('modules.configs.nvim-treesitter'),
-    opts = {
-        -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "c", "robot", "cpp", "python", "bash", "cmake", "lua", "vim", "vimdoc", "query" },
-        -- Install parsers synchronously (only applied to `ensure_installed`)
+    config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
         sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false
-        }
-    }
+        highlight = { enable = true },
+        indent = { enable = true },
+    })
+    end
 }
 
 
