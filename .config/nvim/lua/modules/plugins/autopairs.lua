a509b745d7410e7360c33664649f9d1e4b18eeb3
Evgenii Gostev <gostevevg0@gmail.com>
2024-02-20 13:31:56 +0300

    nvim/autopairs: Delete autopair for backtick in verilog files

--- .config/nvim/lua/modules/plugins/autopairs.lua
+++ .config/nvim/lua/modules/plugins/autopairs.lua
@@ -2,7 +2,9 @@ local autopairs = {}
 
 autopairs['windwp/nvim-autopairs'] = {
     event = "InsertEnter",
-    opts = {} -- this is equalent to setup({}) function
+    opts = {
+    }, -- this is equalent to setup({}) function
+    config = require('modules.configs.autopairs')
 }
 
 
