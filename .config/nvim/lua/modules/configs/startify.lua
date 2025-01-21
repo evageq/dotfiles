108e11533424323576539019bf6e091b3650b4eb
Evgenii Gostev <gostevevg0@gmail.com>
2025-01-21 16:00:16 +0300

    nvim/lua: Change startify bookmarks remarks

--- .config/nvim/lua/modules/configs/startify.lua
+++ .config/nvim/lua/modules/configs/startify.lua
@@ -1,3 +1,8 @@
 return function()
-    vim.g.startify_bookmarks = {'~/.zshrc', '~/.config/nvim/lua/modules', '~/.config/nvim/lua/core/mappings.lua', }
+    vim.g.startify_bookmarks = {
+    { z = '~/.zshrc' },
+    {nv = '~/.config/nvim/lua'},
+    '~/.config/nvim/lua/core/mappings.lua',
+}
+
 end
