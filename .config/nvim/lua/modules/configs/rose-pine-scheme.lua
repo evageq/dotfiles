ee605e8764f902033aec1995f3ffb510cf458465
evageq <gostevevg0@gmail.com>
2025-11-29 16:05:57 +0300

    nvim: Add misc plugins

--- .config/nvim/lua/modules/configs/rose-pine-scheme.lua
+++ .config/nvim/lua/modules/configs/rose-pine-scheme.lua
@@ -1,4 +1,4 @@
 return function()
     require('rose-pine').setup{styles = { italic = false }}
-	vim.cmd('colorscheme rose-pine-moon')
+	vim.cmd('colorscheme rose-pine-main')
 end
--- .config/nvim/lua/modules/configs/telescope.lua
+++ .config/nvim/lua/modules/configs/telescope.lua
@@ -97,4 +97,6 @@ return function()
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    require("telescope").load_extension("undo")
+    vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
 end
